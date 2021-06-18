19d9556e27d8150a4b01b2731499d9228c125848
aaaaayushh <aayushshah1142@gmail.com>
2021-06-18 11:22:43 +0530

    horner's algo implementation

---
+++ transform and conquer/Poly eval/horner.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int horner(int poly[], int n, int x)
+{
+    int result = poly[0];
+
+    for (int i = 1; i < n; i++)
+        result = result * x + poly[i];
+
+    return result;
+}
+
+int main()
+{
+    int poly[] = {2, -6, 2, -1};
+    int x = 3;
+    int n = sizeof(poly) / sizeof(int);
+    cout << "value of polynomial is :" << horner(poly, n, x) << '\n';
+    return 0;
+}
